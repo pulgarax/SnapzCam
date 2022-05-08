c4e805074b4be68e7611f19188222ad32aeb093e
Axel Pulgar <brepundtrep@yahoo.com.ar>
2022-05-08 14:00:46 +0200

    Initial Commit

---
+++ SnapzCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapzCam
+//
+//  Created by Axel Pulgar on 08.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
